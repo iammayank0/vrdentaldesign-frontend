fdc9d79eb185318b4d54d1c5442a619ac9d3c773
Aman Goad <amangoad546@gmail.com>
2024-06-13 16:06:06 +0530

    slider cta

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -613,7 +613,67 @@ const Main = () => {
           </div>
         </div>
       </section>
-
+{/* Doctor Area */}
+<section className="doctor-area">
+        <div className="container-doctor">
+          <div className="section-title">
+            <span className="doctor-title">OUR SERVICES</span>
+            <h2>Check our all Digital Dental Services</h2>
+          </div>
+          <div className="doctor-slides-carousel">
+            <div
+              className="doctor-slides"
+              style={{ transform: `translateX(-${currentDoctorSlide * 100}%)` }}
+            >
+              {doctors.map((doctor, index) => (
+                <div className="single-doctor-box" key={index}>
+                  <div className="doctor-image">
+                    <img src={doctor.img} alt={doctor.title} />
+                  </div>
+                  <div className="doctor-content">
+                    <h3>{doctor.title}</h3>
+                    <span>{doctor.time}</span> {/* Use the 'time' property from the fetched data */}
+                    <ul className="social-icon">
+                      {/* Render social links */}
+                      <li>
+                        <a href={doctor.socialLinks.facebook}>
+                          <FaFacebook />
+                        </a>
+                      </li>
+                      <li>
+                        <a href={doctor.socialLinks.twitter}>
+                          <FaTwitter />
+                        </a>
+                      </li>
+                      <li>
+                        <a href={doctor.socialLinks.linkedin}>
+                          <FaLinkedin />
+                        </a>
+                      </li>
+                      <li>
+                        <a href={doctor.socialLinks.instagram}>
+                          <FaInstagram />
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              ))}
+            </div>
+            <div className="slides-dot">
+              {Array.from({ length: totalDoctorSlides }).map((_, index) => (
+                <button
+                  key={index}
+                  className={`dot ${currentDoctorSlide === index ? "active" : ""}`}
+                  onClick={() => handleDotClick(index)}
+                >
+                  <span>.</span>
+                </button>
+              ))}
+            </div>
+          </div>
+        </div>
+      </section>
 
         </>
       )}
